788d814e59d18bc2921e4732b9cf247b472344d9
Joseph T Lyons <JosephTLyons@gmail.com>
2021-02-17 22:48:32 -0500

    Added setup.py
    Will need more details added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file_handle:
+    long_description = file_handle.read()
+
+setuptools.setup(
+    name="ssh-session-manager",
+    version="0.0.1",
+    author="SYSCON International",
+    author_email="author@example.com",
+    description="Execute commands on multiple remote SSH sessions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Syscon-Plantstar/ssh-session-manager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)

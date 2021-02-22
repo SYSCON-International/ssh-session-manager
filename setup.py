1cdd5ac6f84dbf888ba1d35f80f42fde7d9446af
Joseph T Lyons <JosephTLyons@gmail.com>
2021-02-22 01:07:04 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as file_handle:
 
 setuptools.setup(
     name="ssh-session-manager",
-    version="0.0.2",
+    version="0.0.3",
     author="SYSCON International",
     author_email="dev@syscon-intl.com",
     description="Execute commands on multiple remote SSH sessions",
